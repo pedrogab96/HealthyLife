d0513a2b031d9c06e8c82358820df826273b51d4
pedrogab96 <39196117+pedrogab96@users.noreply.github.com>
2018-05-31 13:08:35 -0300

    Revert "Addicionando viewport"
    This reverts commit bce2ff2ff72826951d29d949d323f13f170de23a.

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    $('.slide-clients').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
